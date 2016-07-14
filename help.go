e633f8e1c756ba8a86f688de687dc831af4cd687
Sarah Harvey <shh@squareup.com>
2016-07-13 21:40:26 -0700

    fixes from testing in production #yolo

--- help.go
+++ help.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-  "golang.org/x/net/websocket"
-  "log"
+	"log"
+
+	"golang.org/x/net/websocket"
 )
 
 // Prints help message
 func doHelp(config Config, ws *websocket.Conn, user string, channel string) {
-  var m Message
+	var m Message
 	m.Type = "message"
 	m.Channel = channel
 
-  m.Text = `start _team name_: sets your team's name and PMs you a link to a puzzle. This starts your clock.
-validate _flag_: tells you if a flag is correct (message or invite me to a private channel first!).
+	m.Text = `start _team name_: sets your team's name and PMs you a link to a puzzle. This starts your clock.
+validate _level_ _flag_: tells you if a flag for a level is correct (message or invite me to a private channel first!).
 scores: tells you the current top scores (beta)`
-  log.Printf("posting: %v", m)
+	log.Printf("posting: %v", m)
 	postMessage(ws, m)
 }
